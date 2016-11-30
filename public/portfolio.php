fe9a3b5185f5e5c6c7cbf322c2f182e17514fa28
lauer.gwendoline@gmail.com <gwendolinelauer@MacBook-Pro-de-Gwendoline.local>
2016-11-30 13:02:01 +0100

    Création de la page d'affichage des images du portfolio (portfolio.php)

---
+++ public/portfolio.php
@@ -0,0 +1,54 @@
+<?php
+
+session_start();
+
+require_once '../include/system/dbConnexion.php';
+require_once '../include/functions.php';
+
+$head = [
+	'title' => 'Portfolio'
+];
+
+
+$images = getImagesPortfolio();
+
+if(isset($_POST['#add-image'])) {
+	header('add-portfolio.php');
+  	exit();
+}
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>$head['title']</title>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="script.js"></script>
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.min.css">
+</head>
+<body>
+
+	<?php 
+		include('inc/header.php');
+	?>
+
+	<h1>Portfolio</h1>
+
+	<!-- Affichage des images du portfolio -->
+	
+	<?php foreach ($images as $image) : ?>
+		<div>
+			<div><img src="../<?=$image['url']; ?>" alt="<?=$image['title']; ?>"></div>
+			<div><p><?=$image['title']; ?></p></div>
+			<div><p><?=$image['caption']; ?></p></div>
+		</div>
+	<?php endforeach; ?>
+
+
+	<button id="add-image" type="button" class="btn btn-primary">Ajouter une image</button>
+
+</body>
+</html>
