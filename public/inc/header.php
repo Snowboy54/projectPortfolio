2d4070d3016281e3a039741f66d35ea3a7726940
Alexandre Caliaro <caliaro.alexandre@gmail.com>
2016-12-01 09:10:51 +0100

    Modif Header

--- public/inc/header.php
+++ public/inc/header.php
@@ -32,14 +32,14 @@ require_once '../include/functions.php';
 				<nav class="navbar navbar-inverse" role="navigation">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
-						<a class="navbar-brand" href="#">Brand</a>
+						<a class="navbar-brand" href="index.php">Brand</a>
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Link</a></li>
-							<li><a href="#">Link</a></li>
+							<li class="active"><a href="portfolio.php">Portfolio</a></li>
+							<li><a href="slider.php">Slider</a></li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
 							<li><a href="#">Link</a></li>
